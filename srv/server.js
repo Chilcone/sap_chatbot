c2e9c2b1d64682b6d03892892f164b2655ab51cf
ericn3 <51316265+ericn3@users.noreply.github.com>
2022-01-23 23:32:16 +0100

    Add product API

--- srv/server.js
+++ srv/server.js
@@ -1,6 +1,7 @@
 const cds = require("@sap/cds");
 const express = require('express');
 const ordersHelper = require('./SalesOrders/orders.js')
+const productsHelper = require('./Products/products.js')
 
 const proxy = require("@sap/cds-odata-v2-adapter-proxy"); //enable OData 2 support
 
@@ -9,6 +10,7 @@ cds.on("bootstrap", (app) => {
     app.use(express.json())
 
     app.post('/orders', async function(req, resp) { ordersHelper.ordersResponse(req, resp) })
+    app.post('/products', async function(req, resp){productsHelper.productsResponse(req, resp)})
 }) 
 
 module.exports = cds.server
