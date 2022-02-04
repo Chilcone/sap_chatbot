358b360ffc6ceed2d65a0c1c37c42c8deb59d7da
zhouh <ge49wej@tum.de>
2022-02-04 12:23:21 +0100

    billing

--- srv/example-service.js
+++ srv/example-service.js
@@ -4,9 +4,15 @@ module.exports = cds.service.impl(async function() {
 	const salesService = await cds.connect.to('OP_API_SALES_ORDER_SRV_0001'); // connect to the external destination
 	const { SalesOrders } = this.entities;
 	const  {SalesOrderOrderItem } = this.entities;
-    
+    const { SaleOrderHeaderPartner } = this.entities;
+
 	const productsService = await cds.connect.to('API_PRODUCT_SRV');
 	const { Products } = this.entities;
+
+	const billingsService = await cds.connect.to('API_BILLING_DOCUMENT_SRV');
+	const { BillingDocuments } = this.entities; 
+
+	
 	this.before('*', (req) => {
         console.debug('>>>', req.method, req.target.name)
     });
@@ -22,5 +28,13 @@ module.exports = cds.service.impl(async function() {
 	this.on('READ', SalesOrderOrderItem, request => {
 		return salesService.tx(request).run(request.query);
 	});
+
+	this.on('READ', SaleOrderHeaderPartner, request => {
+		return salesService.tx(request).run(request.query);
+	});
+
+	this.on('READ', BillingDocuments, request => {
+		return billingsService.tx(request).run(request.query);
+	})
 	
 });
