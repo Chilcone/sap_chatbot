4fefbedc8c2d7dc851f05bb79e66a49c82a89848
zhouh <ge49wej@tum.de>
2022-02-06 11:16:10 +0100

    billings

--- srv/SalesOrders/ordersDate.js
+++ srv/SalesOrders/ordersDate.js
@@ -4,9 +4,18 @@ const utility = require('./utility.js')
 async function ordersDateResponse(req, resp) { 
     let url = "https://" + req.headers.host + '/example/SalesOrders?$filter='
     if('datetime' in req.body.nlp.entities) {
-        let date = req.body.nlp.entities['datetime'][0].iso.split('T')[0]
-        url += 'CreationDate%20ge%20' + date
-    } else { 
+        let startDate = new Date(req.body.nlp.entities['datetime'][0].iso)
+        let accuracy = req.body.nlp.entities['datetime'][0].accuracy
+        var endDate = new Date()
+        if (accuracy.includes("day")) {
+            endDate = new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate()+1)
+        } else if (accuracy.includes("month")) {
+            endDate = new Date(startDate.getFullYear(), startDate.getMonth()+1, startDate.getDate())
+        } else if (accuracy.includes("year")) {
+            endDate = new Date(startDate.getFullYear()+1, startDate.getMonth(), startDate.getDate())
+        }
+        url += 'CreationDate%20ge%20' + startDate.toISOString().split('T')[0] + '%20and%20CreationDate%20le%20' + endDate.toISOString().split('T')[0]
+    } else if ('interval' in req.body.nlp.entities){ 
         console.log(req.body.nlp.entities)
         let startDate = req.body.nlp.entities['interval'][0].begin.split('T')[0]
         let endDate = req.body.nlp.entities['interval'][0].end.split('T')[0]
