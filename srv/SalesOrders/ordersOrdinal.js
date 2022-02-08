f38910610ace19c3eecaf87d89f5f71dbcc2237b
Suekrue Gueltop <suekrue.gueltop@sap.com>
2022-02-09 00:12:07 +0100

    Add Sales Order comments

--- srv/SalesOrders/ordersOrdinal.js
+++ srv/SalesOrders/ordersOrdinal.js
@@ -1,20 +1,50 @@
 const https = require('https');
 const utility = require('./utility.js')
-
-async function ordersOrdinalResponse(req, resp) { 
+/**
+ * Prepares response for the request with sorting information.
+ * Expects ordinal gold entity in entities.
+ * 'The last' has -1 ordinal entity so we used this information to sort the Sales Orders. There is also a 'sort' gold entity in CAI, but it doesn't work as expected. 
+ * And restricted entities block gold entities since overlapped entities are not supported(for example it would block last year to be recognized as interval gold entity), 
+ * so we had to go with ordinal gold entity.
+ * E.g. "Show me my last five sales order" would give -1 as ordinal rank and 5 as id scalar.
+ * 
+ * @param {Request} req Request with ordinal information.
+ * The form of req body:
+ * {
+ *     "action_id": "5149f96e-b2c5-4e30-96ec-c587ae978d52",
+ *     "conversation": {
+ *          "memory": { ... }
+ *     },
+ *     "nlp": {
+ *          "entities": { 
+ *               "ordinal": [ <ordinal entity of the expression> ]
+ *          }
+ *     },
+ *     "api_service_response": {}
+ * }
+ * @param {Response} resp CAI reply response with updated memory.
+ * The form of response:
+ * {
+ *     "replies": [<message according to request>],
+ *     "conversation": <updated(if necessary) conversation information got from request>
+ *  }
+ */async function ordersOrdinalResponse(req, resp) { 
+    // Get the count of the sales order. It is mostly matched to 'id' entity due to trained expressions. 
+    // Default is 1, since "show my last sales order" type of expressions means only one record.
     let count = 1
     if('id' in req.body.nlp.entities) {
         count = req.body.nlp.entities['id'][0].scalar
     }
 
+    // Get the sorting info from ordinal gold entity.
     let ordinal = 'desc'
     if(req.body.nlp.entities['ordinal'][0].rank > 0) { 
         ordinal = 'asc'
     }
-    
-    //let url = "https://" + 'ibsolution-development-tum2021ws-ib-sap-chatbot-srv.cfapps.eu10.hana.ondemand.com' + '/example/SalesOrders?$orderby=CreationDate%20'+ordinal+'&$top='+count
+    // Prepare OData url.
     let url = "https://" + req.headers.host + '/example/SalesOrders?$orderby=CreationDate%20'+ordinal+'&$top='+count
     
+    // Fetch OData response.
     https.get(url, res => {
         let data = []
         res.on('data', chunk => {
@@ -22,12 +52,13 @@ async function ordersOrdinalResponse(req, resp) {
         })
         res.on('end', () => {
             const responseData = JSON.parse(Buffer.concat(data).toString());
-            console.log(responseData)
+             // If there is only one Sales Order, it also updates the memory for further skill navigation.
             if(responseData.value.length == 1) {
                 req.body.conversation.memory.lastOrderID = responseData.value[0].SalesOrder
             }
+            // Prepare messages for response.
             let messages = utility.getMessages(responseData.value)
-
+            // Send response with replies and updated conversation.
             resp.send({
                 "replies": [ messages
                 ],
--- srv/server.js
+++ srv/server.js
@@ -11,13 +11,16 @@ cds.on("bootstrap", (app) => {
     app.use(proxy())
     app.use(express.json())
 
+    // Sales Order CAI webhook endpoints.
     app.post('/orders', async function(req, resp) { ordersHelper.ordersResponse(req, resp) })
     app.post('/orders/ordinal', async function(req, resp) { ordersOrdinalHelper.ordersOrdinalResponse(req, resp) })
     app.post('/orders/date', async function(req, resp) { ordersDateHelper.ordersDateResponse(req, resp) })
 
+    // Products CAI webhook endpoints.
     app.post('/products', async function(req, resp){ productsHelper.productsResponse(req, resp) })
     app.post('/productsFromSalesOrder', async function(req, resp){ productsHelper.productsFromSalesOrder(req, resp) })
     
+    // Billings CAI webhook endpoints
     app.post('/billings', async function(req, resp){ bllingsHelper.billingsResponse(req, resp) })
     app.post('/billings/date', async function(req, resp){ bllingsHelper.billingsDateResponse(req, resp) })
     app.post('/billingsFromSalesOrder',  async function(req, resp){ bllingsHelper.billingsFromSalesOrder(req, resp) })
